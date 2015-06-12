28f3c65f550b3eaea3100e59cfe920ffca88eb11
budgester <budgester@budgester.com>
2015-06-12 21:59:01 +0100

    Configured ec2

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,3 +9,10 @@ validation_client_name   "budgester-validator"
 validation_key           "#{current_dir}/budgester-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/budgester"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+
+knife[:aws_ssh_key_id] = 'budgester'
+
+knife[:aws_access_key_id] = ENV['AWS_ACCESS_KEY_ID'] 
+knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
+knife[:flavor] = "t2.micro"
+knife[:region] = "eu-west-1"
